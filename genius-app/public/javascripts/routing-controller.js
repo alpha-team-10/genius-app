98d7c195962ac3db79fa28034ac4bdf44504f67d
Kolchakov <nikola.kolchakov@gmail.com>
2017-09-30 21:56:52 +0300

    refactor using Promise.all

--- genius-app/public/javascripts/routing-controller.js
+++ genius-app/public/javascripts/routing-controller.js
@@ -7,28 +7,20 @@ let $container = $("#container");
 let routingController = function(dataservice, templateLoader, layoutProvider, utils) {
 
     function listing(name) {
-        let pendingData = null;
+        // get data and templateFunc at once and do the work
+        Promise.all([dataservice.getByName(name), templateLoader.get('listing')])
+            .then(result => {
+                let data = result[0];
+                let funcTemplate = result[1];
 
-        dataservice
-            .getByName(name)
-            .then(data => {
-
-                // save the data for later use
-                pendingData = data;
-
-                // postprocessing
-                pendingData.response.hits.map(hit => {
+                data.response.hits.map(hit => {
                     hit.type = utils.isAlbum(hit) ? "album" : "song";
                     let pageViews = hit.result.stats.pageviews;
                     hit.result.stats.pageviews = utils.numberWithLetter(pageViews);
                 });
 
-                // return the template for listing
-                return templateLoader.get("listing");
-            })
-            .then(funcTemplate => {
-                // compile and attach to DOM
-                let compiledHtml = funcTemplate(pendingData);
+
+                let compiledHtml = funcTemplate(data);
                 layoutProvider.partialLayout($container, compiledHtml);
             });
     }
