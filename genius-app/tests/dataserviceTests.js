2f4dfba76225fbc824c9019e06f9f12080752e84
Ditmar Vladislavov <didox222@gmail>
2017-10-09 13:12:32 +0300

    unit test for getSongById and getHTML added

--- genius-app/tests/dataserviceTests.js
+++ genius-app/tests/dataserviceTests.js
@@ -1,6 +1,6 @@
 describe("dataservice", function () {
 
-    it("should call $.get", function () {
+    it("getByName should call $.ajax", function () {
 
         let fakeData = new Promise((resolve, reject) => {
             resolve({ result: "any song" })
@@ -13,5 +13,33 @@ describe("dataservice", function () {
         dataservice.getByName(jasmine.anything());
 
         expect($.ajax).toHaveBeenCalled();
+    });
+
+    it("getSongById shuold call $.ajax", function() {
+        let fakeData = new Promise((resolve, reject) => {
+            resolve({ result: "any id" })
+        });
+
+        spyOn($, "ajax")
+        .and
+        .returnValue(fakeData)
+
+        dataservice.getSongById(jasmine.anything());
+
+        expect($.ajax).toHaveBeenCalled();
+    });
+
+    it("getHTML shuold call $.get", function() {
+        let fakeData = new Promise((resolve, reject) => {
+            resolve({ result: "any url" })
+        });
+
+        spyOn($, "get")
+        .and
+        .returnValue(fakeData)
+
+        dataservice.getHTML(jasmine.anything());
+
+        expect($.get).toHaveBeenCalled();
     });
 })
